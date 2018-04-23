0a529c21e09528f71b5dd96321427d69e8d90e3a
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-23 15:45:32 +0200

    Unity 5.6.3

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,7 +4,7 @@
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
 echo 'Downloading Unity 2017.3.1f1 pkg:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/a2913c821e27/MacEditorInstaller/Unity-5.6.2f1.pkg
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/d3101c3b8468/MacEditorInstaller/Unity-5.6.3f1.pkg
 # http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
 # https://download.unity3d.com/download_unity/497a0f351392/MacEditorInstaller/Unity-5.6.0f3.pkg
 # https://download.unity3d.com/download_unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
