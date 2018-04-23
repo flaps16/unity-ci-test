4a2a2e75b94788aac404459a18387ff59615c94a
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-23 14:18:35 +0200

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,7 +3,7 @@
 # Download Unity3D installer into the container
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 5.5.1 pkg:'
+echo 'Downloading Unity 2017.3.1f1 pkg:'
 curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
