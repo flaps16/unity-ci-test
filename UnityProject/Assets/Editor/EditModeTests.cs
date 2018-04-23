bf1e9021010adf3d4615fe633b829557df902c78
Anders Geer <geer.anders@gmail.com>
2018-04-23 15:24:23 +0200

    Removed the [UnityTest]

--- UnityProject/Assets/Editor/EditModeTests.cs
+++ UnityProject/Assets/Editor/EditModeTests.cs
@@ -17,12 +17,4 @@ public class EditModeTests {
         Assert.True(false);
     }
 
-    // A UnityTest behaves like a coroutine in PlayMode
-    // and allows you to yield null to skip a frame in EditMode
-    [UnityTest]
-	public IEnumerator EditModeTestsWithEnumeratorPasses() {
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		yield return null;
-	}
 }
