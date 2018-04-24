6a2d4571d16098ee647634c3ade63d93a371af22
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-24 12:55:45 +0200

    Original tests - original project

---
+++ UnityProject/Assets/ConnectToServerSript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ConnectToServerSript : MonoBehaviour {
+	public NetworkManager manager;
+	private SteamVR_TrackedController _controller;
+
+
+	private void OnEnable()
+	{
+		_controller = GetComponent<SteamVR_TrackedController>();
+		_controller.TriggerClicked += HandleTriggerClicked;
+	}
+	
+	private void OnDisable()
+	{
+		_controller.TriggerClicked -= HandleTriggerClicked;
+	}
+	
+	private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+	{
+		Connect();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		manager = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManager>();
+	}
+
+	private void Connect()
+	{
+		manager.StartClient();
+	}
+}
---
+++ UnityProject/Assets/DevScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+
+public class DevScript : MonoBehaviour
+{
+    public event Action<bool> SpaceClicked; 
+    
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public virtual void OnSpaceClicked(bool e)
+    {
+        if (SpaceClicked != null) SpaceClicked.Invoke(e);
+    }
+}
