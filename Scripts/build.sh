f0a2e00b15c6274af2f72efb3174154c3ba13281
Anders Geer Jakobsen <geer.anders@gmail.com>
2018-04-24 17:05:59 +0200

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -15,7 +15,7 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
 	-runTests \
 	-testResults $(pwd)/test.xml \
-	-testPlatform playmode
+	-testPlatform playmode \
 	-testPlatform editmode
 #	-quit
 
