e54418f6929be3718cf58d08f42f4853c678cb9b
Anders Geer <geer.anders@gmail.com>
2018-04-24 13:28:45 +0200

    Revert "-"
    This reverts commit a76f98161898aa6beae7c5715baf2b704d892645.

---
+++ UnityProject/Assets/Editor/EditModeTests.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class EditModeTests {
+
+    [Test]
+    public void EditModeTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions.
+    }
+    //[Test]
+    public void EditModeTestsSimplePassesFail()
+    {
+        Assert.True(false);
+    }
+
+}
